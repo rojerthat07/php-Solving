1911451afd894962254daf2b4ee1123c8c312437
Jer Carlo Catallo <49897728+rojerthat07@users.noreply.github.com>
2020-03-17 16:12:56 +0800

    Mumbling
    Examples:
    
    accum("abcd") -> "A-Bb-Ccc-Dddd"
    accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
    accum("cwAt") -> "C-Ww-Aaa-Tttt"
    
    Link: https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/php

--- index.php
+++ index.php
@@ -3,9 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>php Problem Solving</title>
+    <link rel="icon" href="images/php-logo.png">
 </head>
 <body>
-    <?php echo("jer") ?>
+    <?php
+    //This time no story, no theory. The examples below show you how to write function accum:
+    //Problem Link - https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/php
+    function accum($s) {
+        $finalOutput = "";
+    
+    
+        for($i = 0; $i != strlen($s);$i++,$j++){
+            for($j = 0 ;$j <= $i;$j++){
+                if($j > 0){
+                    $finalOutput .= strtolower($s[$i]) ;
+                } else {
+                    $finalOutput .= strtoupper($s[$i]);
+                }
+            }
+            if($i != strlen($s) -1){
+                $finalOutput .= "-";
+            } 
+        }
+      
+       return $finalOutput;
+    }
+
+
+  accum('EvidjUnokmM');
+    
+    
+    ?>
+
+
+
 </body>
 </html>
