aff9121e2ae2e5aeb5568fafd970de6207c90748
Jer Carlo Catallo <49897728+rojerthat07@users.noreply.github.com>
2020-04-02 16:56:52 +0800

    refine Detect Pangram

--- index.php
+++ index.php
@@ -15,8 +15,6 @@
         $alphabet = array("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
         $numberOfAlphabet = count($alphabet);
         $numberOfChecks = 0;
-        $checker = false;
-        $newString = "";
 
         for($i =0 ;$i < strlen($string);$i++){
             $numberOfAlphabet = count($alphabet);
